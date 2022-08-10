c07362f41aa38c4a735aedf73e5df00cfdcf2cd2
Developer <110471703+Qurolboyev-Azamjon@users.noreply.github.com>
2022-08-10 15:55:38 +0500

    qoshish

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+function myBars(){
+    document.querySelector('.navbar__hide').style.left = '0';
+    document.querySelector('body').style.overflow = 'hidden';
+}
+
+function myXmark(){   
+    document.querySelector('.navbar__hide').style.left = '100%';
+}
